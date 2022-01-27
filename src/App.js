6bc5d347926723a9c0e290b2206f032789eac998
Adriana <adriana.parkova@rwth-aachen.de>
2022-01-27 14:00:45 +0200

    LoginForm created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
+import LoginForm from "./LoginForm"; 
 
 function App() {
   return (
     <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
+      <section className="hero">
+        <div className="hero-body">
+          <p className="title">A React Task</p>
+          <p className="subtitle">by Boom.dev</p>
         </div>
       </section>
-      <div class="container is-fullhd">
-        <div class="notification">
+      <div className="container is-fullhd">
+        <div className="notification">
           Edit the <code>./src</code> folder to add components.
+          <LoginForm/>
         </div>
       </div>
     </div>
