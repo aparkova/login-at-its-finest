ed1bca6a8e9514b5056364cdbe9ffc7e6e9f4782
Adriana <adriana.parkova@rwth-aachen.de>
2022-01-27 15:40:25 +0200

    form with labels and inputs

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -4,16 +4,14 @@ import styles from './components/LoginForm.module.css'
 function LoginForm(){
     return(
     <form className={styles.form}>
-            <label className={styles.label}>Name
-              <input className={styles.input} type="text" name="name"></input>
-            </label>
-            <div></div>
-            <label className={styles.label}>Password
-              <input className={styles.input} type="password" name="password" ></input>
-            </label>
-            <div></div>
-            <button className={styles.submit} type="submit">Submit</button>
-          </form>
+        <label className={styles.label}>Name</label>
+        <input className={styles.input} type="text" name="name"></input>
+        <div></div>
+        <label className={styles.label}>Password</label>
+        <input className={styles.input} type="password" name="password" ></input>
+        <div></div>
+        <button className={styles.submit} type="submit">Submit</button>
+    </form>
     )
 }
 
