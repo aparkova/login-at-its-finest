cd42710208be7fb38bb6d4ae14f5b9f939221cbc
Adriana Parkova <adriana.parkova@gmail.com>
2022-02-03 21:35:10 +0100

    try again

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,10 +5,10 @@ function LoginForm(){
     return(
     <form className={styles.form}>
         <label className={styles.label} for="text">Name
-            <input className={styles.input} type="text" id="text"/>     
+            <input className={styles.input} type="text" name="text"/>     
         </label>
         <label className={styles.label} for="password">Password
-            <input className={styles.input} type="password" id="password"/>
+            <input className={styles.input} type="password" name="password"/>
         </label>
         <input className={styles.submit} type="submit" value="Submit"/>
     </form>
