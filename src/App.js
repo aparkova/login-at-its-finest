219c72aeb4a7a10efc66f588a4c95cdb69f481ca
Adriana Parkova <adriana.parkova@gmail.com>
2022-02-03 21:48:10 +0100

    try again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import LoginForm from "./LoginForm"; 
+import LoginForm from "./components/LoginForm.js"; 
 
 function App() {
   return (
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './components/LoginForm.module.css'
+import styles from './LoginForm.module.css'
 
 function LoginForm(){
     return(
